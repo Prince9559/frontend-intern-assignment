07d262e2823b00bb0827e20fdf8cd763ea63f928
Prince Software <kumarprince95596186@gmail.com>
2026-02-27 13:27:54 +0530

    project

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,14 @@
 import "./TaskCard.css";
 
-const TaskCard = ({ title, desc }) => {
+const TaskCard = ({ title, desc, onDelete }) => {
   return (
     <div className="task-card">
       <h3>{title}</h3>
       <p>{desc}</p>
+
+      <button className="delete-btn" onClick={onDelete}>
+        Delete
+      </button>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,27 @@ import Footer from "../components/Footer";
 import TaskCard from "../components/TaskCard";
 
 const Home = () => {
-  const [tasks] = useState([
+  const [tasks, setTasks] = useState([
     { title: "HTML", desc: "Markup structure" },
     { title: "CSS", desc: "Styling and layout" },
-    { title: "JavaScript", desc: "Logic handling" },
-    { title: "React", desc: "Component based UI" },
   ]);
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title || !desc) return alert("Fill all fields");
+
+    setTasks([...tasks, { title, desc }]);
+    setTitle("");
+    setDesc("");
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
 
   return (
     <>
@@ -18,12 +33,27 @@ const Home = () => {
       <main className="container">
         <h2>Frontend Intern Assignment</h2>
 
+        <form onSubmit={handleSubmit} className="task-form">
+          <input
+            placeholder="Task Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            placeholder="Task Description"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button>Add Task</button>
+        </form>
+
         <div className="task-grid">
           {tasks.map((task, index) => (
             <TaskCard
               key={index}
               title={task.title}
               desc={task.desc}
+              onDelete={() => deleteTask(index)}
             />
           ))}
         </div>
